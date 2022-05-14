a06b689bbd651b7318005d9730c85886aac8c319
hldingzydong <847394047@qq.com>
2022-05-14 15:17:59 +0800

    thread switch done

--- user/uthread.c
+++ user/uthread.c
@@ -10,12 +10,36 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+// Saved registers for user context switches.
+struct thread_context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 
 struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
+  char                  stack[STACK_SIZE];       /* the thread's stack */
+  int                   state;                   /* FREE, RUNNING, RUNNABLE */
 
+  void                  (*func)();               /* execute function */
+  struct thread_context thread_context;          /* saved register when switch */
 };
+
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
@@ -63,6 +87,8 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    // printf("switch from thread %d to thread %d\n", t - all_thread, current_thread - all_thread);
+    thread_switch((uint64)&t->thread_context, (uint64)&current_thread->thread_context);
   } else
     next_thread = 0;
 }
@@ -77,11 +103,15 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->func = func;
+  t->thread_context.ra = (uint64)t->func;
+  t->thread_context.sp = (uint64)(t->stack + STACK_SIZE);
 }
 
 void 
 thread_yield(void)
 {
+  // printf("thread %d call yield\n", current_thread - all_thread);
   current_thread->state = RUNNABLE;
   thread_schedule();
 }
